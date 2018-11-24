c1cfa2c33b69695b50d748dc94f7175a149c5da7
Toufiqur Chowdhury <htr.letun@gmail.com>
2018-11-25 01:55:23 +0600

    Added/updated sample configuration files

--- example-start.sh
+++ example-start.sh
@@ -1,23 +1,5 @@
-# Discord bot settings
-export BotToken=""
-export CommandPrefix="!"
-export BotRootUserID=""
-
-## Set API URLs used by the Halo Info Bot
-# Halo Platform
-export URL_DEX_REST=""
-export URL_DEX_GQL=""
-export URL_Explorer_REST=""
-export URL_Mainnet_GQL=""
-export URL_MNDApp_REST=""
-
-# CoinMarketCap.com
-export URL_CMC_REST=""
-export API_KEY_CMC=""
-
-# Etherscan.io
-export URL_Etherscan_REST=""
-export API_Key_Etherscan=""
-
+# If Go is installed
 #go run *.go
+
+# Otherwise, if on Linux based systems
 ./builds/halo-info-bot-linux64
